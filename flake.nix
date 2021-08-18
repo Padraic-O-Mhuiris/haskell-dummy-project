57673be08982f20c13aade949ae7a5b0b7ac02c1
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-19 00:57:07 +0100

    using ghc 8.10.5

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
             # This overlay adds our project to pkgs
             helloProject = final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc8104";
+              compiler-nix-name = "ghc8105";
               # This is used by `nix develop .` to open a shell for use with
               # `cabal`, `hlint` and `haskell-language-server`
               shell.tools = {
